e6016c1661e5d78cabc32a1af71d6c0a026ce549
Jonathan Gamon <jonathaneg51995@gmail.com>
2025-03-05 16:21:31 -0300

    portafolio subido

---
+++ js/cv-download.js
@@ -0,0 +1,21 @@
+const BOTON_DESCARGA_CV = document.getElementById("CV_BUTTON");
+
+function descargarArchivo() {
+    // URL del archivo que quieres que se descargue
+    const url = '../CV/mi_cv.pdf';  // Asegúrate de que esta ruta sea correcta
+    const nombreArchivo = 'CV_Jonathan_Gamon.pdf';  // Cambia la extensión a .pdf
+
+    // Crear un elemento <a> para activar la descarga
+    const enlace = document.createElement('a');
+    enlace.href = url;
+    enlace.download = nombreArchivo;
+
+    // Simular un clic en el enlace para iniciar la descarga
+    document.body.appendChild(enlace);
+    enlace.click();
+
+    // Eliminar el enlace del DOM después de la acción
+    document.body.removeChild(enlace);
+}
+
+BOTON_DESCARGA_CV.addEventListener("click", () => descargarArchivo());
